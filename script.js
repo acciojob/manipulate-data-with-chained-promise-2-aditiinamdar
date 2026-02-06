336d34c158c70e481b9f047fea6ed471819afe71
Aditi Inamdar <66082809+aditiinamdar@users.noreply.github.com>
2026-02-06 23:42:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+// your JS code here
+const outputDiv = document.getElementById("output");
+
+function returnArray() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 3000);
+  });
+}
+
+returnArray()
+  .then(function (arr) {
+    const evens = arr.filter((nums) => nums % 2 === 0);
+
+    return new Promise(function (resolve) {
+      setTimeout(() => {
+        outputDiv.textContent = evens;
+        resolve(evens);
+      }, 1000);
+    });
+  })
+  .then(function (evens) {
+    const multiplied = evens.map((num) => num * 2);
+
+    return new Promise(function (resolve) {
+      setTimeout(() => {
+        outputDiv.textContent = multiplied;
+        resolve(multiplied);
+      }, 2000);
+    });
+  });
